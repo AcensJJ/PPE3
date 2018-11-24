2eb8bab776dea67625ae966556d1ed15d1346942
AcensJJ <uentosacens@gmail.com>
2018-11-24 19:27:26 +0100

    inscription css // Quelque pb mais convenable

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -2,9 +2,9 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategorieRepository")
@@ -22,6 +22,11 @@ class Categorie
      * @ORM\Column(type="string", length=255)
      */
     private $libelle;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Produit", mappedBy="categorieProduit")
+     */
+    private $produits;
 
     public function __construct()
     {
@@ -41,6 +46,37 @@ class Categorie
     public function setLibelle(string $libelle): self
     {
         $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Produit[]
+     */
+    public function getProduits(): Collection
+    {
+        return $this->produits;
+    }
+
+    public function addProduit(Produit $produit): self
+    {
+        if (!$this->produits->contains($produit)) {
+            $this->produits[] = $produit;
+            $produit->setCategorieProduit($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduit(Produit $produit): self
+    {
+        if ($this->produits->contains($produit)) {
+            $this->produits->removeElement($produit);
+            // set the owning side to null (unless already changed)
+            if ($produit->getCategorieProduit() === $this) {
+                $produit->setCategorieProduit(null);
+            }
+        }
 
         return $this;
     }
